a1f92f530eb39c65c472d1e87bb1d4da52866fc3
luisdavid159 <luis.patino@uab.edu.bo>
2025-08-03 19:03:14 -0400

    Crear seeder para tabla marcas

---
+++ database/seeders/MarcaSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Marca;
+class MarcaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Marca::create(['nombre' => 'Samsung']);
+        Marca::create(['nombre' => 'Apple']);
+        Marca::create(['nombre' => 'Huawei']);
+    }
+}
