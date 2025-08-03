173d64e43868fbc432ee72b1ea1e63844bf7b108
luisdavid159 <luis.patino@uab.edu.bo>
2025-08-03 19:08:28 -0400

    Crear modelo Producto con relación a Marca

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+     use HasFactory;
+
+    protected $fillable = ['nombre', 'precio', 'marca_id'];
+
+    public function marca()
+    {
+        return $this->belongsTo(Marca::class);
+    }
 }
